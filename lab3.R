9c72f1ea514be8c25790289fb08320afea1f4ec6
Nelson Javier  Romero Yepez <nelsonry@TTC.CAT>
2023-11-22 12:35:00 +0100

    Pregunta 1.3: se creo un dataset filtrando por imagen

--- lab3.R
+++ lab3.R
@@ -31,3 +31,10 @@ con_error <- dplyr::filter(errores_serv, between (codigo,400,999))
 colnames (con_error) <- c('host', 'codigo', 'conteo')
 errores_serv_resumen <- con_error %>% group_by(codigo) %>% mutate(codigo) %>% summarise(n = n())
 colnames(errores_serv_resumen) <- c('codigo', 'Conteo')
+
+#pregunta 1.3
+pregunta_1_3 <- dplyr::select(epa_http, url, tipo, timestamp)
+pregunta_1_3_solo_img <- dplyr::select(endsWith(pregunta_1_3$url,'.png'))
+pregunta_1_3_solo_img <- pregunta_1_3 %>% filter(url %like% "(.png|.jpg|.gif|.JPG|.PNG|.GIF)")
+
+
